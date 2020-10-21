791e9c6c5ad807fb9f5efd9541e9b29f5096a38b
vrnchll <veronichka127@mail.ru>
2020-10-21 18:48:19 +0300

    feat: add TowerSort task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let arr = [];
+    if (matrix === undefined) {
+        return [];
+    } 
+        for (i = 0; i < matrix.length; i++)
+		{
+        if (i == 0 || i % 2 == 0)  arr.push(matrix[i]);
+        else  arr.push(matrix[i].reverse());
+        }
+return [].concat(...arr);
 }
